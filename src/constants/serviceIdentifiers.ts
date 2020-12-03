bc292db972d33abfb033aeaf355c3b76e99d7bac
James Oliver <joliver.robert@me.com>
2020-12-03 11:13:47 +0000

    adds Inversify constants

---
+++ src/constants/serviceIdentifiers.ts
@@ -0,0 +1,13 @@
+const serviceIdentifiers = {
+  EmployeeTypeORM: Symbol("EmployeeTypeORM"),
+  DepartmentTypeORM: Symbol("DepartmentTypeORM"),
+  TitleTypeORM: Symbol("TitleTypeORM"),
+  EmployeeRepository: Symbol("EmployeeRepository"),
+  DepartmentRepository: Symbol("DepartmentRepository"),
+  TitleRepository: Symbol("TitleEntityRepository"),
+  EmployeesService: Symbol("EmployeesService"),
+  DepartmentsService: Symbol("DepartmentsService"),
+  TitlesService: Symbol("TitlesService"),
+};
+
+export default serviceIdentifiers;
